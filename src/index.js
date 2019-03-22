446b0d43fa92e3840bacdddd88140f258486e25e
youngeffort <893520964@qq.com>
2019-03-22 14:31:36 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,61 @@
+/* eslint-disable
+  global-require,
+  no-param-reassign,
+  prefer-destructuring,
+  import/no-dynamic-require,
+*/
+import { getOptions } from 'loader-utils';
+import validateOptions from 'schema-utils';
+import mime from 'mime';
+
+import normalizeFallback from './utils/normalizeFallback';
+import schema from './options.json';
+console.log(schema)
+
+// Loader Mode
+export const raw = true;
+
+export default function loader(src) {
+  // Loader Options
+  const options = getOptions(this) || {};
+
+  validateOptions(schema, options, 'URL Loader');
+
+  const file = this.resourcePath;
+  // Set limit for resource inlining (file size)
+  let limit = options.limit;
+
+  if (limit) {
+    limit = parseInt(limit, 10);
+  }
+  // Get MIME type
+  const mimetype = options.mimetype || mime.getType(file);
+
+  // No limit or within the specified limit
+  if (!limit || src.length < limit) {
+    if (typeof src === 'string') {
+      src = Buffer.from(src);
+    }
+
+    return `module.exports = ${JSON.stringify(
+      `data:${mimetype || ''};base64,${src.toString('base64')}`
+    )}`;
+  }
+
+  // Normalize the fallback.
+  const {
+    loader: fallbackLoader,
+    options: fallbackOptions,
+  } = normalizeFallback(options.fallback, options);
+
+  // Require the fallback.
+  const fallback = require(fallbackLoader);
+
+  // Call the fallback, passing a copy of the loader context. The copy has the query replaced. This way, the fallback
+  // loader receives the query which was intended for it instead of the query which was intended for url-loader.
+  const fallbackLoaderContext = Object.assign({}, this, {
+    query: fallbackOptions,
+  });
+
+  return fallback.call(fallbackLoaderContext, src);
+}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        index: path.resolve('./src/index.js'),
+    },
+    output: {
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+        path: path.resolve('./dist/'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /.js$/,
+            loader: 'babel-loader'
+        }]
+    },
+    node: {
+        fs: "empty"
+    }
+};
